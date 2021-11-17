d2dbdfee9432a827e30926fb13234ab75379bb7e
Ammann Tobias <tammann@ergonomics.ch>
2021-11-17 23:38:41 +0100

    WIP

--- src/main/java/axon/AxonConfig.java
+++ src/main/java/axon/AxonConfig.java
@@ -1,15 +1,16 @@
-package axon.util;
+package axon;
 
+import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventsourcing.AggregateLoadTimeSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AxonConfig {
-
     @Bean
     public SnapshotTriggerDefinition cardSnapshotTrigger(Snapshotter snapshotter) {
         return new AggregateLoadTimeSnapshotTriggerDefinition(snapshotter, 10);
@@ -20,4 +21,8 @@ public class AxonConfig {
         return new EventCountSnapshotTriggerDefinition(snapshotter, 30);
     }
 
+    @Autowired
+    public void configureSubscribingProcessors(EventProcessingConfigurer processingConfigurer) {
+        processingConfigurer.registerSubscribingEventProcessor("ticket-events-policy");
+    }
 }
