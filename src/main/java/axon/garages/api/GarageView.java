19ae5eab137eccdf3de2cafd13411ed97f5cc17e
Ammann Tobias <tammann@ergonomics.ch>
2021-11-17 21:27:13 +0100

    WIP

--- src/main/java/axon/garages/api/GarageView.java
+++ src/main/java/axon/garages/api/GarageView.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class GarageView implements Comparable<GarageView> {
-    String gid;
+    String garageId;
     Integer capacity;
     Integer used;
 
